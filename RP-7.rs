a63743a97f8a32043766595cc960d6d73b3f38d8
GUBBALA SANTOSH KUMAR <gubbalasantoshkumar2004@gmail.com>
2025-07-21 10:21:04 +0530

    Create RP-7.rs

---
+++ RP-7.rs
@@ -0,0 +1,11 @@
+//Create and Access Tupples
+fn main() {
+    let tupple = (0, 2, 4, 6, 8, 10);
+    println!("Tupple Values");
+    println!("{}",tupple.0);
+    println!("{}",tupple.1);
+    println!("{}",tupple.2);
+    println!("{}",tupple.3);
+    println!("{}",tupple.4);
+    println!("{}",tupple.5);
+}
