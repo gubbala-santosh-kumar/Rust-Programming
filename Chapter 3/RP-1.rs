f4ddeac3f836dce111ecdd656a80af4a7b431658
GUBBALA SANTOSH KUMAR <gubbalasantoshkumar2004@gmail.com>
2025-07-27 22:19:18 +0530

    Create RP-1.rs

---
+++ Chapter 3/RP-1.rs
@@ -0,0 +1,11 @@
+// 1. Write a program to check if a number is positive or negative
+fn main() {
+    let num = -10;
+    if num > 0 {
+        println!("Positive.");
+    } else if num < 0 {
+        println!("Negative.");
+    } else {
+        println!("Zero.");
+    }
+}
