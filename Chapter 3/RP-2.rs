e5dd04de8bdca82f9fba920de23b81ff6b282bce
GUBBALA SANTOSH KUMAR <gubbalasantoshkumar2004@gmail.com>
2025-07-27 22:21:26 +0530

    Create RP-2.rs

---
+++ Chapter 3/RP-2.rs
@@ -0,0 +1,10 @@
+//2. Write a program to determine if a number is even or odd
+fn main(){
+  let num = 10;
+  if num%2==0{
+    println!("Even Number");
+  }
+  else{
+    println!("Odd Number");
+  }
+}
